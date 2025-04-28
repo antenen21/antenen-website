dd93c15ced249fba08562a98e05575bcad15f85b
antenen21 <info.antenen@gmail.com>
2025-04-28 20:41:28 +0200

    ✨ F01: Learn to commit right

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  /// state (etat, données)
+  const [companyName, setCompanyName] = useState("Antenen Gebäudetechnik")
+  const [count, setsetCount] = useState(200000)
 
+  /// comportements
+
+  const handleClick = () => {
+    alert("handleClick")
+
+    setsetCount(count + 1)
+    
+
+  }
+
+  /// render
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1 className="p-4 bg-red-100">{companyName}</h1>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>Increament</button>
+    </div>
+  );
 }
 
 export default App
