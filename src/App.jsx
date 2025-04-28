60203fb44de71f4bcb012957865293932a2d44f0
antenen21 <info.antenen@gmail.com>
2025-04-28 21:54:14 +0200

    ✨ F01: LOGIN > create LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,14 @@ import { useState } from "react"
 
 function App(){
   /// state (etat, données)
-  const [companyName, setCompanyName] = useState("Antenen Gebäudetechnik")
-  const [count, setsetCount] = useState(200000)
 
   /// comportements
 
-  const handleClick = () => {
-    alert("handleClick")
-
-    setsetCount(count + 1)
-    
-
-  }
 
   /// render
   return (
     <div>
-      <h1 className="p-4 bg-red-100">{companyName}</h1>
-      <h1>{count}</h1>
-      <button onClick={handleClick}>Increament</button>
+      <h1>Login Page</h1>{" "}
     </div>
   );
 }
