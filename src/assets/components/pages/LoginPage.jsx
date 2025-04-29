57beaa74ad7f5de15a924bc39b8e40174af919f8
antenen21 <info.antenen@gmail.com>
2025-04-29 15:27:48 +0200

    ✨ F01: LOGIN > Add title and form

--- src/assets/components/pages/LoginPage.jsx
+++ src/assets/components/pages/LoginPage.jsx
@@ -1,3 +1,30 @@
+import { useState } from "react"
+
+
 export default function LoginPage() {
-  return <h1>Antenen website</h1>
+  /// state
+  const [inputValue, setInputValue] = useState("")
+
+  /// comportement
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    alert(`Bonjour ${inputValue}`)
+    setInputValue("")
+  }
+
+  const handleChange = (event) => { 
+    setInputValue(event.target.value)
+   }
+
+
+  /// render
+  return <div>
+    <h1>Bienvenue chez nous!</h1>
+    <br />
+    <h2>Connectez-vous</h2>
+    <form action="submit" onSubmit={handleSubmit}>
+    <input value={inputValue} onChange={handleChange} type="text" placeholder="Entrez votre prénom" required/>
+    <button >accédez à votre éspace</button>
+    </form>
+  </div>
 }
